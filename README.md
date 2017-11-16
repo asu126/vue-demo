f2f62c853e1f76a8bba98f354c1cc2f2ec6b3c2c
33 <suguojun126@163.com>
2017-11-16 22:17:58 +0800

    fix error use ace edit

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ We decided to move Vue Version to separate repository. You can find all files he
 # Flex 布局教程 http://www.ruanyifeng.com/blog/2015/07/flex-examples.html
 
 # highligtht.js https://segmentfault.com/a/1190000008188461
+
+# ace编辑器部分参考了使用了该[brace gem包](https://github.com/thlorenz/brace)，并参考了[vue2-ace-editor](https://github.com/chairuosen/vue2-ace-editor)与vue2的整合方法.
--- src/views/vue_edit.vue
+++ src/views/vue_edit.vue
@@ -1,35 +1,33 @@
 <template>
   <div>
-     <editor id="javascript-editor" lang="html" theme="chrome" height="500">
-     </editor>
+    <editor v-model="content" lang="html" theme="chrome" height="300" ></editor>
+    </editor>
+    <button v-on:click="testFunc">点我打印输入内容</button>
   </div>
 </template>
 
 <script>
+import AceEdit from './ace/ace_index.js'
 require('brace')
 require('brace/mode/javascript')
 require('brace/theme/monokai')
+require('brace/theme/chrome')
 require('brace/mode/html')
-
-// var editor = ace.edit('javascript-editor')
-// editor.getSession().setMode('ace/mode/javascript')
-// editor.setTheme('ace/theme/monokai')
 
 export default {
   name: 'AceEdit',
-  data: {
-    content: '111'
+  data () {
+    return {
+      content: '111'
+    }
   },
   components: {
-    editor: require('vue2-ace-editor')
+    editor: AceEdit
+  },
+  methods: {
+    testFunc: function () {
+      console.log(this.content)
+    }
   }
-  // methods: {
-  //   initEditor: function (editor) {
-  //     ace.require('brace/mode/html')
-  //     ace.require('brace/mode/php')
-  //     ace.require('brace/theme/chrome')
-  //     ace.require('brace/theme/twilight')
-  //   }
-  // }
 }
 </script>
